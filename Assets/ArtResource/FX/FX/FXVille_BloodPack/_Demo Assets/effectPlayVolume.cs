b0ae795ce99035495aa2a9948667cb2b7a76ab65
Tony <tony020409@gmail.com>
2019-07-03 12:32:03 +0800

    2131

---
+++ Assets/ArtResource/FX/FX/FXVille_BloodPack/_Demo Assets/effectPlayVolume.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class effectPlayVolume : MonoBehaviour 
+{
+	public ParticleSystem effectToPlay;
+	public bool armed = false;
+
+	void OnTriggerEnter(Collider other)
+	{
+		armed = true;
+	}	
+
+	void OnTriggerExit(Collider other)
+	{
+		armed = false;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(armed && Input.GetKeyDown(KeyCode.Space))
+		{
+			if(effectToPlay.isPlaying == true)
+			{
+				effectToPlay.Clear();
+				effectToPlay.Play();
+			}
+			else
+			{
+				effectToPlay.Play();
+			}
+		}
+	}
+}
---
+++ Assets/ArtResource/FX/FX/FXVille_BloodPack/_Demo Assets/trailEffectTestObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trailEffectTestObject : MonoBehaviour 
+{
+	public float speed = 0.5f;
+	public float width = 5f;
+	public float height = 1f;
+	public float wiggleFreq = 4f;
+
+	public Vector3 centerPosition;
+
+	public float localTime = 0f;
+	// Use this for initialization
+	void Start () 
+	{
+		centerPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		localTime += Time.deltaTime * speed;
+
+		Vector3 tempPos = Vector3.zero;
+		tempPos.x = Mathf.Sin (localTime) * width;
+		tempPos.z = Mathf.Cos (localTime) * width;
+		tempPos.y = Mathf.Sin (localTime * wiggleFreq) * height;
+
+		transform.localPosition = tempPos + centerPosition;
+	}
+}
